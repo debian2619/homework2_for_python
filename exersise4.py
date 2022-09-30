0eb963390f28a95f65f7b59fd035289fac00e3a2
Dmitry <ya.potapov@yandex.ru>
2022-09-30 17:39:13 +0300

    exersise4.py

---
+++ exersise4.py
@@ -0,0 +1,20 @@
+# Задание 4 Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на позициях a и b.
+# Значения N, a и b вводит пользователь с клавиатуры.
+
+
+def create_sequence(n):
+    result =  []
+    for i in range(-n, n + 1 ):
+        result.append(i)
+    return result
+
+def multiplication(a, b, lst):
+   return lst[a - 1] * lst [b - 1]
+
+n = int(input('N: '))
+a = int(input('a: '))
+b = int(input('b: '))
+res_list = create_sequence(n)
+    
+print(f'a * b = {res_list[a - 1]} * {res_list[b - 1]} = {multiplication(a, b, res_list)}')
